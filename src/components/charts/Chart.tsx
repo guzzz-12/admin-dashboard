246d1a8b488b7a5e3b2db07b5b022cba5ccba5cd
J3duardo <jegq.dev.projects@gmail.com>
2022-08-07 19:26:20 -0400

    Componente Chart y sus dependencias para renderizar el gráfico

---
+++ src/components/charts/Chart.tsx
@@ -0,0 +1,73 @@
+import {Box, Typography} from "@mui/material";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from "recharts";
+import "./charts.css";
+
+interface Chart {
+  name: string;
+  total: number;
+}
+
+const CHART_DATA: Chart[] = [
+  {name: "January", total: 1200},
+  {name: "February", total: 2100},
+  {name: "March", total: 900},
+  {name: "April", total: 1500},
+  {name: "May", total: 750},
+  {name: "June", total: 1800},
+  {name: "July", total: 1200}
+];
+
+const Chart = () => {
+  return (
+    <Box className="chart">
+      <Typography className="chart__title" variant="h5">
+        Last 7 months revenue
+      </Typography>
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          style={{paddingTop:"var(--spacing-lg)", fontFamily: "Nunito"}}
+          data={CHART_DATA}
+          width={100}
+          height={100}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          {/* <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="cornflowerblue" stopOpacity={0.8}/>
+              <stop offset="95%" stopColor="#fff" stopOpacity={0}/>
+            </linearGradient>
+          </defs> */}
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="4 4" />
+          <Tooltip
+            wrapperStyle={{
+              textTransform: "capitalize",
+              outline: "none",
+              border: "1px solid grey"
+            }}
+          />
+          <Area
+            type="monotone"
+            dataKey="total"
+            stroke="cornflowerblue"
+            strokeWidth={2}
+            fillOpacity={0.5}
+            fill="cornflowerblue"
+            // fill="url(#total)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </Box>
+  )
+}
+
+export default Chart;
