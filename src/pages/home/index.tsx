dfd43692f3104c7b7ef131f538dd5c84eb1b17f2
J3duardo <jegq.dev.projects@gmail.com>
2022-08-06 20:51:12 -0400

    Mostrar grid de widgets en el home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,19 @@
-import { Typography } from "@mui/material";
+import {Box} from "@mui/material";
 import Layout from "../../components/Layout";
+import Widget from "../../components/widget";
+import {WIDGETS_DATA} from "../../components/widget/widgetData";
 import "./home.css";
 
 const HomePage = () => {
   return (
     <Layout>
-      <section className="home">
-        <Typography className="home__title" variant="h2">
-          Home page
-        </Typography>
-      </section>
+      <Box className="home" component="section">
+        <Box className="home__widgets">
+          {WIDGETS_DATA.map((item, i) => {
+            return <Widget key={i} widgetData={item} />
+          })}
+        </Box>
+      </Box>
     </Layout>
   )
 }
