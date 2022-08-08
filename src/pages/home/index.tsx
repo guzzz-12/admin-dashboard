a96b12d2a946fee5d587414d54cef82abc215404
J3duardo <jegq.dev.projects@gmail.com>
2022-08-07 21:30:19 -0400

    No renderizar el layout en el home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,4 @@
 import {Box} from "@mui/material";
-import Layout from "../../components/Layout";
 import Widget from "../../components/widget";
 import Chart from "../../components/charts/Chart";
 import FeaturedChart from "../../components/charts/FeaturedChart";
@@ -9,22 +8,20 @@ import "./home.css";
 
 const HomePage = () => {
   return (
-    <Layout>
-      <Box className="home" component="section">
-        <Box className="home__widgets">
-          {WIDGETS_DATA.map((item, i) => {
-            return <Widget key={i} widgetData={item} />
-          })}
-        </Box>
-        <Box className="home__charts">
-          <FeaturedChart />
-          <Chart />
-        </Box>
-        <Box className="home__transactions">
-          <TransactionsTable />
-        </Box>
+    <Box className="home" component="section">
+      <Box className="home__widgets">
+        {WIDGETS_DATA.map((item, i) => {
+          return <Widget key={i} widgetData={item} />
+        })}
       </Box>
-    </Layout>
+      <Box className="home__charts">
+        <FeaturedChart />
+        <Chart />
+      </Box>
+      <Box className="home__transactions">
+        <TransactionsTable />
+      </Box>
+    </Box>
   )
 }
 
