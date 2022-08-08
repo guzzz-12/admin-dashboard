ce09e83af1d5fc1ac73e674174d0c26a8a819239
J3duardo <jegq.dev.projects@gmail.com>
2022-08-07 21:32:29 -0400

    Páginas products y users

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,14 @@
+import {Box, Typography} from "@mui/material";
+import "./products.css";
+
+const Products = () => {
+  return (
+    <Box className="products">
+      <Typography>
+        Products page
+      </Typography>
+    </Box>
+  )
+}
+
+export default Products;
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,14 @@
+import {Box, Typography} from "@mui/material";
+import "./users.css";
+
+const UsersPage = () => {
+  return (
+    <Box className="users">
+      <Typography>
+        Users Page
+      </Typography>
+    </Box>
+  )
+}
+
+export default UsersPage;
